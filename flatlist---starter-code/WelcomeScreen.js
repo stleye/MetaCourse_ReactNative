39a38ecee444533d8b3f57f4e3379c79cbbf2b84
Sebastian Tleye <stleye@gmail.com>
2024-02-28 21:01:02 -0300

    Added flatlist exercise

--- flatlist---starter-code/WelcomeScreen.js
+++ flatlist---starter-code/WelcomeScreen.js
@@ -1,24 +1,20 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, Text, StyleSheet } from 'react-native';
 
 export default function WelcomeScreen() {
   return (
-    <View style={ welcomeStyles.container }>
-      <Text
-        style={ welcomeStyles.headerText }>
-        Welcome to Little Lemon
-      </Text>
-      <Text
-        style={ welcomeStyles.regularText }>
+    <ScrollView indicatorStyle="white" style={styles.container}>
+      <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+      <Text style={styles.regularText}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear more about your experience with us!
       </Text>
-    </View>
+    </ScrollView>
   );
 }
 
-const welcomeStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -34,5 +30,5 @@ const welcomeStyles = StyleSheet.create({
     marginVertical: 8,
     color: '#EDEFEE',
     textAlign: 'center',
-  }
-})
+  },
+});
--- flatlist---starter-code/components/LittleLemonFooter.js
+++ flatlist---starter-code/components/LittleLemonFooter.js
@@ -3,24 +3,23 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function LittleLemonFooter() {
   return (
-    <View
-      style={footerStyles.container}>
-      <Text
-        style={footerStyles.footerText}>
+    <View style={styles.container}>
+      <Text style={styles.footerText}>
         All rights reserved by Little Lemon, 2022{' '}
       </Text>
     </View>
   );
 }
 
-const footerStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#EE9972'
+    backgroundColor: '#EE9972',
+    marginBottom: 20,
   },
   footerText: {
     fontSize: 18,
     color: 'black',
     textAlign: 'center',
     fontStyle: 'italic',
-  }
-})
+  },
+});
